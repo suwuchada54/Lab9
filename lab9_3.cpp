4ed4f100514c2d4729976381956abb9f623317ef
suwichada <jippee.1211@gmail.com>
2021-12-07 13:28:25 +0700

    lab9_3.cpp

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,5 +1,4 @@
-/*
-"Enter your age: "
+/*"Enter your age: "
 "Enter your height: "
 "Enter your property: "
 "Your status = "
@@ -7,5 +6,42 @@
 "FRIEND"
 "BEST FRIEND"
 "ONE-NIGHT-STAND"
-"MARRIED"
-*/
+"MARRIED"*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age,height,property;
+    cout<<"Enter your age: ";
+    cin>>age;
+    if(age<=20){
+        cout<<"Enter your height: ";
+        cin>>height;
+        if(height<160){
+          cout<<"Your status = UNFRIEND"; 
+        }else if(height<175&&height>=160){
+            cout<<"Your status = FRIEND";
+        }else if(height>=175){
+            cout<<"Enter your property: ";
+            cin>>property;
+            if(property<=69000000){
+              cout<<"Your status = ONE-NIGHT-STAND"; 
+            }else{
+                cout<<"Your status = MARRIED";
+            }
+        }
+    }else if(age<30&&age>20){
+        cout<<"Enter your property: ";
+            cin>>property;
+            if(property>10000000){
+              cout<<"Your status = BEST FRIEND"; 
+            }else{
+                cout<<"Your status = UNFRIEND";
+            }
+    }else{
+        cout<<"Your status = UNFRIEND";
+    }
+ 
+  return 0;
+}
